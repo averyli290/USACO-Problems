e64794ec68c9ef3eebbaf6d56ee5d02d84729337
averyli290 <averyli408@gmail.com>
2017-09-16 15:27:14 -0700

    Solved and updated a problem solution

--- SilverLevelUSACO/silver_prob1feb2015/silver_prob1feb2015.cpp
+++ SilverLevelUSACO/silver_prob1feb2015/silver_prob1feb2015.cpp
@@ -7,5 +7,38 @@
 using namespace std;
 
 int main() {
-	cout << "Hello World!" << endl;
+	ifstream fin ("censor.in");
+	ofstream fout ("censor.out");
+	
+	string s = " ";
+	string c = " ";
+	fin >> s >> c;
+	
+	vector<char> a;
+	vector<char> b;
+	for(int i = 0; i < s.length(); i++) {
+		a.push_back(s[i]);
+	}
+
+	for(int i = 0; i < c.length(); i++) {
+		b.push_back(c[i]);
+	}
+	int i = 0;;
+	for(int i = 0; i < a.size()- c.length(); i++) {
+		bool match = true;
+		for(int j = 0; j < c.length(); j++) {
+			if(s[i+j] != c[j]) {
+				match = false;
+			}
+		}
+		if(match) {
+			a.erase(a.begin() + i - 2, a.begin() + c.length() + i);
+			i = 0;
+		}
+	}
+
+	for(int i = 0; i < a.size(); i++) {
+		cout << a[i] << ", ";
+	}
+	cout << endl;
 }
