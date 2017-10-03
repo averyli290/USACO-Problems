382f94ba86fcd4563e6f30d9493d355f2fb7b969
averyli290 <averyli408@gmail.com>
2017-10-02 20:35:09 -0700

    sdf

--- SilverLevelUSACO/silver_prob3feb2015_unfinished/silver_prob1feb2015.cpp
+++ SilverLevelUSACO/silver_prob3feb2015_unfinished/silver_prob1feb2015.cpp
@@ -6,9 +6,12 @@
 
 using namespace std;
 
+char arr[15][15];
+
 int t = 0;
 
-int movecow(int xb, int yb, int cx, int cy, char cc, arr) {
+int movecow(int xb, int yb, int cx, int cy, char cc) {
+	//cout << cx << " " << cy << endl;
 	if(cx == xb && cy == yb) {
 		return 1;
 	} else {
@@ -17,8 +20,9 @@ int movecow(int xb, int yb, int cx, int cy, char cc, arr) {
 		}
 		for(int i = cx; i <= xb; i++) {
 			for(int j = cy; j <= yb; j++) {
-				if(arr[i][j] != cc) {
-					return t + movecow(xb, yb, i, j, arr[i][j], arr);
+				if (movecow(xb, yb, i, j, arr[i][j]) && arr[i][j] != cc) {
+					t++;
+					return 1;
 				}
 			}
 		}
@@ -30,13 +34,13 @@ int main() {
 	ofstream fout ("hopscotch.out");
 
 	int r, c = 0;
-	char hopfield[r][c];
+	fin >> r >> c;
 	for(int a = 0; a < r; a++) {
 		for(int b = 0; b < c; b++) {
-			fin >> hopfield[a][b];
+			fin >> arr[a][b];
 		}
 	}
 	
-	movecow(r-1, c-1, 0, 0, hopfield[0][0], hopfield);
-	return t;
+	cout <<  movecow(r-1, c-1, 0, 0,arr[0][0]) << endl; 
+	cout << t << endl;
 }
